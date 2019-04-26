55740a2f189cb7f47638f5fc550b0a27b9110267
Christopher Entwistle <mara_uk@hotmail.com>
2019-04-26 15:24:26 +0100

    Added a barrel reference and aiming component
    - Added an aiming component for aiming
    - Added a barrel reference to get the barrel

--- BattleTanks/Source/BattleTanks/Public/Tank.h
+++ BattleTanks/Source/BattleTanks/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -15,17 +16,21 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	// Sets the barrel mesh reference
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent * BarrelToSet);
+
 	void AimAt(FVector HitLocation);
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UTankAimingComponent * TankAimingComponent = nullptr;
 };
