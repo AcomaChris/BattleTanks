6ee53cc9152defa4cf2fe9a6ee9c04b8097227dd
Christopher Entwistle <mara_uk@hotmail.com>
2019-04-27 13:30:04 +0100

    Added pitch rotation with clamping on values
    - Added pitch rotation to barrel via FMath::Clamp
    - Takes min/max elevation
    - Moves barrel to correct location in pitch based on projectile velocity

--- BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankBarrel.cpp
@@ -8,5 +8,16 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 	// Work out difference between current barrel rotation and aim direction
 	// Move the barrel the right amount this frame, given the max elevation speed and frame time
 
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.0f, 1.0f);
+
+	// Find the elevation change we want to make
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+
+	// Set the barrel elevation to the new pitch
+	float RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	float Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+
+
+	SetRelativeRotation(FRotator(Elevation, 0.0f, 0.0f));
 
 }
