b72ca955c9b6833c000fd0902c950e24795c7251
Christopher Entwistle <mara_uk@hotmail.com>
2019-05-06 17:11:50 +0100

    Added tank track inputs, components, and forces (plus quit button)
    - Added tank track component in C+
    - Added tank track inputs
    - Added ApplyForceAtLocation() to tracks via inputs
    - Added a quit button

---
+++ BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankTrack.h"
+
+// Sets the throttle for the attached track
+void UTankTrack::SetThrottle(float Throttle)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle is at: %f"), *Name, Throttle);
+
+	// TODO clamp actual throttle value so player cannot over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
